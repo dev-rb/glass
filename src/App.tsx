37ee3a3ba801b6404160133db355cfdd7d5d0ff3
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-12-03 19:55:59 -0500

    Delete default content and styles. Add custom titlebar.

--- src/App.tsx
+++ src/App.tsx
@@ -13,43 +13,12 @@ function App() {
 	}
 
 	onMount(() => {
-		invoke('add_decorations');
+		document
+			.getElementById('titlebar')
+			?.addEventListener('contextmenu', (e) => e.preventDefault());
 	});
 
-	return (
-		<div class="container">
-			<h1>Welcome to Tauri!</h1>
-
-			<div class="row">
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" class="logo vite" alt="Vite logo" />
-				</a>
-				<a href="https://tauri.app" target="_blank">
-					<img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-				</a>
-				<a href="https://solidjs.com" target="_blank">
-					<img src={logo} class="logo solid" alt="Solid logo" />
-				</a>
-			</div>
-
-			<p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-			<div class="row">
-				<div>
-					<input
-						id="greet-input"
-						onChange={(e) => setName(e.currentTarget.value)}
-						placeholder="Enter a name..."
-					/>
-					<button type="button" onClick={() => greet()}>
-						Greet
-					</button>
-				</div>
-			</div>
-
-			<p>{greetMsg}</p>
-		</div>
-	);
+	return <div></div>;
 }
 
 export default App;
