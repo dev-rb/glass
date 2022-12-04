daba17030479dfd76e11c1540927e267e354fc50
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-12-03 20:58:38 -0500

    Add custom context menu for titlebar. Add clickoutside listener. Add sample image for testing. Update body and root styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import { createSignal, onMount } from 'solid-js';
+import { createSignal, JSX, onMount, Show } from 'solid-js';
 import logo from './assets/logo.svg';
 import { invoke } from '@tauri-apps/api/tauri';
 import './App.css';
+import { Portal } from 'solid-js/web';
 
 function App() {
-	const [greetMsg, setGreetMsg] = createSignal('');
-	const [name, setName] = createSignal('');
+	const [showMenu, setShowMenu] = createSignal<{ x: number; y: number } | null>(
+		null
+	);
+	const [ref, setRef] = createSignal<HTMLElement>();
 
-	async function greet() {
-		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-		setGreetMsg(await invoke('greet', { name: name() }));
-	}
+	const openImage = () => {};
+
+	const clickOutside = (e: MouseEvent) => {
+		if (e.target instanceof HTMLElement) {
+			console.log(e.target);
+			if (ref() && (ref()?.contains(e.target) || ref()?.isSameNode(e.target)))
+				return;
+
+			setShowMenu(null);
+		}
+	};
 
 	onMount(() => {
+		document.addEventListener('mousedown', clickOutside);
 		document
 			.getElementById('titlebar')
-			?.addEventListener('contextmenu', (e) => e.preventDefault());
+			?.addEventListener('contextmenu', (e) => {
+				e.preventDefault();
+				setShowMenu({ x: e.clientX, y: e.clientY });
+			});
 	});
 
-	return <div></div>;
+	return (
+		<div>
+			<Show when={showMenu()}>
+				<Portal mount={document.getElementById('titlebar')!}>
+					<div
+						ref={setRef}
+						id="menu"
+						class="context-menu"
+						style={{
+							top: `${showMenu()?.y ?? 0}px`,
+							left: `${showMenu()?.x ?? 0}px`,
+						}}
+					>
+						<button onClick={openImage}> Open Image </button>
+					</div>
+				</Portal>
+			</Show>
+		</div>
+	);
 }
 
 export default App;
